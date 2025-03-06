e0616b8f643f1c4590662562e1fa6854e1923256
Savi Oumar <oumar.savi@etudiant.univ-rennes.fr>
2025-03-06 13:48:44 +0100

    make views and urls

--- backend/admissions/views.py
+++ backend/admissions/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Institution, Formation, Candidature
+from .serializers import InstitutionSerializer, FormationSerializer, CandidatureSerializer
+
+class InstitutionViewSet(viewsets.ModelViewSet):
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+class FormationViewSet(viewsets.ModelViewSet):
+    queryset = Formation.objects.all()
+    serializer_class = FormationSerializer
+
+class CandidatureViewSet(viewsets.ModelViewSet):
+    queryset = Candidature.objects.all()
+    serializer_class = CandidatureSerializer
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,16 @@ from django.urls import path
 #Authentication
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+#for Api
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import InstitutionViewSet, FormationViewSet, CandidatureViewSet
+
+
+router = DefaultRouter()
+router.register(r'institutions', InstitutionViewSet)
+router.register(r'formations', FormationViewSet)
+router.register(r'candidatures', CandidatureViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +37,7 @@ urlpatterns = [
     #Urls for authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/', include(router.urls)),
 ]
+
